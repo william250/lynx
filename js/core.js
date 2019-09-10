69cb96d821e1b51cad83b99dfe3d9dc8e1189a1b
Will <william@vendedoor.com.br>
2019-09-09 22:00:35 -0300

    initial test files

---
+++ js/core.js
@@ -0,0 +1,13 @@
+let version = "0.0.1",
+    lynx = = function( selector, context ) {
+		return new lynx.fn.init( selector, context );
+	},
+
+class Lynx (
+    constructor(){
+        this.version;
+    }
+    get (){
+
+    }
+)
